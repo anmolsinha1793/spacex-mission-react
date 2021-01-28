b7c05d9112cc4bf3dbecaa172ed9777cb3029062
asinha <m1039308@mindtree.com>
2021-01-28 11:28:48 +0530

    made changes for redirect

--- spacex-launch/src/container/MissionsSpaceX.js
+++ spacex-launch/src/container/MissionsSpaceX.js
@@ -1,25 +1,33 @@
 import React, { Component } from "react";
 import Years from "../components/Years/Years";
 import Missions from "../components/Missions/Missions";
-import classes from './MissionsSpaceX.module.scss';
-import { Route } from "react-router";
+import classes from "./MissionsSpaceX.module.scss";
+import { Route, Switch, Redirect } from "react-router";
 
 class MissionSpaceX extends Component {
-state = {
-    missions: []
-};
-render() {
+  state = {
+    missions: [],
+  };
+  render() {
     return (
-        <>
+      <>
         <div className={classes.MissionContainer}>
-        <div>
-          <Years />
+          <div>
+            <Years />
+          </div>
+          <div>
+            <Switch>
+              <Route
+                path="/missions"
+                render={(props) => (
+                  <Missions {...props} missions={this.state.missions} />
+                )}
+              />
+              <Redirect from="/" to="/missions"></Redirect>
+            </Switch>
+          </div>
         </div>
-        <div>
-          <Route path='/missions' render={(props) => <Missions {...props} missions={this.state.missions}/>}/>
-        </div>
-      </div>
-        </>
+      </>
     );
   }
 }
